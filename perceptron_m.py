54ef6ef28d2e93aa8c447e0f5ea64345cbcf87c5
MickHardins <MickHardins@users.noreply.github.com>
2017-11-17 17:04:53 +0100

    mick's initial commit

---
+++ perceptron_m.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+# activation function sigmoid
+# deriv per poter utilizzare in futuro la f'(net)
+def sigmoid(x, deriv=False):
+    a = 1
+    if (deriv == True):
+        return x * (1 - x)
+    return 1 / (1 + np.exp(-x))
+
+def net_func(x, weights):
+    return np.dot(x, weights)
+
+def out(net, activation):
+    return (activation(net))[0]
+    
+def error(target, output):
+    return (target-output)
+
+def delta(error, net):
+    return -2 * sigmoid(net, True)
+    
+
+x = np.array([[1,2,3,4,5,6]])
+#y = np.array([[1,2,3,4,5,6]])
+
+#print('x+y',x+y)
+weights = np.random.rand(1,6).T     #T = trasposta
+print('weights',weights)
+y = 8
+eta = 0.02
+err = [5.0]
+i = 0
+print(type(err[0]))
+for i in range(1000):
+    i = i+1
+    print('iterazione ', i)
+    net = net_func(x, weights)
+    #print('net', net)
+    output = out(net, sigmoid)
+    err = error(y , output)**2
+    #print('err',err)
+    delt = delta(err, net)
+    print('delta',delt)
+    #print('old', weights)
+    weights = weights  + eta*delt*x
+    print('error', err)
+    #print(eta*delt)
+    #print(eta*delt*x)
+#print('new',weights)
+print('weights',weights)
+print()
+
+
+
+
+
+
+
+
