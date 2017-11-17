9f1d3cbb64d3ac3076086ff99597bcc7531fc0d2
MickHardins <MickHardins@users.noreply.github.com>
2017-11-17 18:42:20 +0100

    perceptron_linear_treshold

---
+++ perceptron_linear_treshold.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+def net_func(x, weights):
+    result = np.dot(x, weights)
+    return result[0][0]
+    
+def activation_function(net):
+    if net > 0:
+        return 1
+    else:
+        return -1
+
+def out(net, activation):
+    return activation(net)
+    
+def error(target, output):
+    return (target-output)
+
+def update_weights(weights, train, target):
+    target = input_vector[1]
+    if (output == input_vector[1]):
+        return weights
+    else:
+        return weights + eta * target * train
+        
+
+train = np.array([[0, 0, 1],
+                  [1, 0, 1],
+                  [0, 1, 1],
+                  [1, 1, 1]])
+target_and = np.array([0, 0, 0, 1])
+target_or = np.array([0, 1, 1, 1])
+
+weights = np.random.rand(1,3).T
+eta = 0.02 #oracolo lavinia 
+
+weights_new = update_weights(weights,train,target)
+
+
+
+
+
+
--- perceptron_m.py
+++ perceptron_m.py
@@ -10,16 +10,18 @@ def sigmoid(x, deriv=False):
     return 1 / (1 + np.exp(-x))
 
 def net_func(x, weights):
-    return np.dot(x, weights)
+    result = np.dot(x, weights)
+    return result[0][0]
 
 def out(net, activation):
-    return (activation(net))[0]
+    return (activation(net))
     
 def error(target, output):
     return (target-output)
 
 def delta(error, net):
-    return -2 * sigmoid(net, True)
+    
+    return -2 * error * sigmoid(net, True)
     
 
 x = np.array([[1,2,3,4,5,6]])
@@ -32,8 +34,12 @@ y = 8
 eta = 0.02
 err = [5.0]
 i = 0
+net = net_func(x, weights)
+
+print(net)
+print(type(sigmoid(net,True)))
 print(type(err[0]))
-for i in range(1000):
+for i in range(2):
     i = i+1
     print('iterazione ', i)
     net = net_func(x, weights)
@@ -41,7 +47,7 @@ for i in range(1000):
     output = out(net, sigmoid)
     err = error(y , output)**2
     #print('err',err)
-    delt = delta(err, net)
+    delt = delta(error(y,output), net)
     print('delta',delt)
     #print('old', weights)
     weights = weights  + eta*delt*x
@@ -51,11 +57,5 @@ for i in range(1000):
 #print('new',weights)
 print('weights',weights)
 print()
-
-
-
-
-
-
 
 
