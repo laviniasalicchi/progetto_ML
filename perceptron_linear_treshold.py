0cf8fc3c50b4be11b7401bb8b6e91ce546ee66e6
MickHardins <MickHardins@users.noreply.github.com>
2017-11-18 23:11:50 +0100

    perceptron funzionante

--- perceptron_linear_treshold.py
+++ perceptron_linear_treshold.py
@@ -1,10 +1,16 @@
+# ==============================================================================
+# E' un Perceptron, che cazzo ti aspettavi.
+#
+# © 2017 Michele Resta & Lavinia Salicchi
+# ==============================================================================
+
 import numpy as np
 
-
 def net_func(x, weights):
-    result = np.dot(x, weights[0])
+    result = np.dot(x, weights)
     return result
-    
+
+#Linear threshold unit
 def activation_function(net):
     if net > 0:
         return 1
@@ -13,85 +19,57 @@ def activation_function(net):
 
 def out(net, activation):
     return activation(net)
-    
-def error(target, output):
-    return (target-output)
 
-#funzione non usata
-def update_weights(weights, train, target):
-    target = input_vector[1]
-    if (output == input_vector[1]):
-        return weights
-    else:
-        weights_new = weights + eta * target * train
-        return weights
-        
+def error(target, output):
+    return (target - output)
+
 def training(train, target, weights, eta):
-    
+
     epoch_weights = weights
-    
     for i in range(0, len(train[:,0])):
         #print(train[i])
         net = net_func(train[i], epoch_weights)
         print('net:\t',net)
         output = activation_function(net)
-        if (activation_function(net) == target[i]):
+        if (output == target[i]):
             #do nothing
-            a = 1
-            #print('pesi immutati')
+            print('pesi immutati', output - target[i])
             #return weights
         else:
-            weights_new = epoch_weights + eta * train[i] * (target[i] - output)
-            epoch_weights = weights_new
-            print('weights_new', weights_new)
+            epoch_weights_new = np.add(epoch_weights, np.multiply(eta * (target[i] - output), train[i]))
+            epoch_weights = epoch_weights_new
+            print('epoch_weights_new', epoch_weights_new)
     return epoch_weights
 
 '''
 -1 corrisponde all'uscita logica 0
 è necessario usare -1 e 1 per evitare che i pesi siano sempre positivi
-            '''
+'''
 train = np.array([[0, 0, 1],
                   [1, 0, 1],
                   [0, 1, 1],
                   [1, 1, 1]])
-target_and = np.array([-1, -1, -1, 1]) 
+target_and = np.array([-1, -1, -1, 1])
 target_or = np.array([-1, 1, 1, 1])
 
-#weights = np.random.rand(1,3)
-weights = np.zeros((1,3), dtype=np.int)
-eta = 0.02      #oracolo lavinia 
+#starting_weights = (np.random.rand(1,3))[0]
+starting_weights = np.zeros((1,3), dtype=np.int)[0]
+eta = 0.02     #oracolo lavinia
 
-print(weights[0])
 i = 0
-while i < 1000: #in teoria sarebbe finchè i pesi non cambiano
-    weights_new = training(train, target_and, weights, eta)
-    if np.array_equal(weights, weights_new):
-        print('training ended')
+while True: #in teoria sarebbe finchè i pesi non cambiano
+    weights_new = training(train, target_or, starting_weights, eta)
+    if np.array_equal(starting_weights, weights_new):
+        print('Ho capito! Ho capito! Basta!')
         break
-    weigths = weights_new
+    starting_weights = weights_new
     print('epoch:', i)
     i += 1
 
-
 #test del modello
 for i in range(0, len(train[:,0])):
-    print('Expected:', 1 if target_and[i] > 0 else 0)
-    net = net_func(train[i], weights)
+    print('Expected:', 1 if target_or[i] > 0 else 0, end='')
+    net = net_func(train[i], starting_weights)
     out = activation_function(net)
-    print('Predicted:', 1 if out > 0 else 0 )
-    
-
-
-'''while True:
-    weights_new = training(train, target_and, weights, eta)
-    if (np.array_equal(weights, weights_new)):
-        print('La smetti di presentarmi sempre la stessa tabella? La so a memoria, cazzo!')
-        break
-    weights = weights_new'''
-
-
-
-
-
-
-
+    print('\tPredicted:', 1 if out > 0 else 0,end='')
+    print()
