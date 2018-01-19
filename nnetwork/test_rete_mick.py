c6058d8115eec12e9c546e1c36f9268eab0bdc24
laviniasalicchi <lavinia.salicchi@libero.it>
2018-01-19 16:25:05 +0100

    .

--- nnetwork/test_rete_mick.py
+++ nnetwork/test_rete_mick.py
@@ -8,6 +8,7 @@ from output_layer import OutputLayer
 from neural_net import NeuralNetwork
 from monk_dataset import *
 from ML_CUP_dataset import ML_CUP_Dataset
+import cross_validation
 
 
 def __main__():
@@ -27,19 +28,21 @@ def __main__():
     output_layer = OutputLayer(1)
     output_layer.create_weights(hidden_layer.n_units)
     output_layer.set_activation_function('sigmoid')
-
+    
     neural_net = NeuralNetwork()
     neural_net.define_loss('mean_euclidean')
     neural_net.add_input_layer(input_layer)
     neural_net.add_hidden_layer(hidden_layer)
     neural_net.add_output_layer(output_layer)'''
 
-    neural_net = NeuralNetwork.create_network(3, 17, 2, 1, 'sigmoid')
+    neural_net = NeuralNetwork.create_network(3, 17, 2, 1, 'sigmoid', slope=1)
 
     monk_datas = MonkDataset.load_encode_monk('/Users/mick/Dati/Università/Pisa/Machine_learning/Prj_info/Progetto_ml/monks-1.train')
     monk_targets = monk_datas[0]
     monk_input = monk_datas[1]
     neural_net.train_network(monk_input, monk_targets, 500, 0.00, 'mean_squared_err', eta=0.3, alfa=0.5, lambd=0.01)
+
+    cross_validation.kfold_cv(monk_input, monk_targets, 500, 0.0, 'mean_squared_err', eta=0.3, alfa=0.5, lambd=0.01)
 
 
 
