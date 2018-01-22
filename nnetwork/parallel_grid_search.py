0c1629519eb90abc8346e140636200a2e7674837
MickHardins <MickHardins@users.noreply.github.com>
2018-01-22 12:43:31 +0100

    aggiunta funzione per caricare dataset monk già codificato con 1-0fk

--- nnetwork/parallel_grid_search.py
+++ nnetwork/parallel_grid_search.py
@@ -21,8 +21,11 @@ def __main__():
         monk_datas = MonkDataset.load_encode_monk('../datasets/monks-1.train')
         monk_targets = monk_datas[0]
         monk_input = monk_datas[1]
+        print(monk_targets.shape)
+        print(monk_input.shape)
+        input()
         start = time.time() * 1000  # benchmark
-        res = start_grid_search(monk_input, monk_targets, 10, 0.0, 'mean_squared_err')
+        res = start_grid_search(monk_input, monk_targets, 600, 0.0, 'mean_squared_err')
 
         #grid_search(monk_input, monk_targets, 1000, 0.0, 'mean_squared_err')
 
@@ -46,7 +49,7 @@ def start_grid_search(input_vect, target_vect, epochs, threshold, loss_func):
     alfas = [0.5, 0.7, 0.9]
     lambds = [0.01, 0.04, 0.07, 0.1]
     n_total_layers = [3, 4, 5]
-    n_hidden_units = [3, 5, 10 ] # range(5, 20)
+    n_hidden_units = range(5, 20)
     act_func = ['sigmoid', 'tanh']
     # creo l'executor a cui mandare i task
     executor = mp.Pool(processes=20)
@@ -59,7 +62,7 @@ def start_grid_search(input_vect, target_vect, epochs, threshold, loss_func):
                     for a in alfas:
                         for l in lambds:
                             print("GridSearch started eta=%f, alfas=%f, lambda=%f", e, a, l)
-                            key = "eta=" + str(e) + " alfa=" + str(a) + " lambda" +str(l) + " ntl=" + str(ntl) + "nhu=" + str(nhu) + "act=" + af + "\t"
+                            key = "eta=" + str(e) + " alfa=" + str(a) + " lambda" +str(l) + " ntl=" + str(ntl) + " nhu=" + str(nhu) + " act=" + af + "\t"
                             #res[key] = executor.apply_async(kfold_task, (input_vect, target_vect, epochs, threshold, loss_func, e, a, l))
                             res[key] = executor.apply_async(kfold_task_net_topology,
                              (input_vect, target_vect, epochs, threshold, loss_func, e, a, l, ntl, nhu, af))
--- nnetwork/test_rete_mick.py
+++ nnetwork/test_rete_mick.py
@@ -28,21 +28,22 @@ def __main__():
     output_layer = OutputLayer(1)
     output_layer.create_weights(hidden_layer.n_units)
     output_layer.set_activation_function('sigmoid')
-    
+
     neural_net = NeuralNetwork()
     neural_net.define_loss('mean_euclidean')
     neural_net.add_input_layer(input_layer)
     neural_net.add_hidden_layer(hidden_layer)
     neural_net.add_output_layer(output_layer)'''
 
-    neural_net = NeuralNetwork.create_network(3, 17, 2, 1, 'sigmoid', slope=1)
+    neural_net = NeuralNetwork.create_network(5, 17, 5, 1, 'sigmoid', slope=1)
 
-    monk_datas = MonkDataset.load_encode_monk('/Users/mick/Dati/Università/Pisa/Machine_learning/Prj_info/Progetto_ml/monks-1.train')
+    monk_datas = MonkDataset.load_monk('/Users/mick/Dati/Università/Pisa/Machine_learning/Prj_info/Progetto_ml/progetto_ML/datasets/monks-1_encoded.train')
     monk_targets = monk_datas[0]
     monk_input = monk_datas[1]
-    neural_net.train_network(monk_input, monk_targets, 2000, 0.00, 'mean_squared_err', eta=0.1, alfa=0.7, lambd=0.01, final=True)
 
-    monk_test = MonkDataset.load_encode_monk('/Users/mick/Dati/Università/Pisa/Machine_learning/Prj_info/Progetto_ml/monks-1.test')
+    neural_net.train_network(monk_input, monk_targets, 500, 0.00, 'mean_squared_err', eta=0.1, alfa=0.9, lambd=0.01, final=True)
+
+    monk_test = MonkDataset.load_monk('/Users/mick/Dati/Università/Pisa/Machine_learning/Prj_info/Progetto_ml/progetto_ML/datasets/monks-1_encoded.test')
     monk_test_target = monk_test[0]
     monk_test_input = monk_test[1]
 
